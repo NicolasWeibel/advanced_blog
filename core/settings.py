51646ca09ff7456f7d67a41ee6e9aef984221ef0
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-11-10 01:40:03 -0300

    Change settings

--- core/settings.py
+++ core/settings.py
@@ -21,6 +21,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DOMAIN = os.environ.get("DOMAIN")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/

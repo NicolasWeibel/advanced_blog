22dfd10caa5b44a7491d13c0b354c7907df49e81
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-11-19 02:29:18 -0300

    Create ContactCreateView

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Contact
+from django.core.mail import send_mail
+
+
+class ContactCreateView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        data = self.request.data
+
+        name = data["name"]
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        phone = data["phone"]
+        budget = data["budget"]
+
+        try:
+            send_mail(
+                subject,
+                "New Client Request: "
+                + "\n\nName: "
+                + name
+                + "\nEmail: "
+                + email
+                + "\n\nMessage:\n"
+                + message
+                + "\nPhone: "
+                + phone
+                + "\n\nBudget: "
+                + budget,
+                "nico.weibel09@gmail.com",
+                ["nico.weibel09@gmail.com"],
+                fail_silently=False,
+            )
+
+            Contact.objects.create(
+                name=name,
+                email=email,
+                phone=phone,
+                subject=subject,
+                message=message,
+                budget=budget,
+            )
+
+            return Response({"success": "Message sent successfully"})
+        except:
+            return Response({"error": "Message not sent"})
